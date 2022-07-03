ce87653bb747f4008ce0d4cf3c4516d6dd363c1d
csj <370916854@qq.com>
2022-07-03 17:25:02 +0800

    first commit

---
+++ src/main/java/com/csj/git/gitTest.java
@@ -0,0 +1,12 @@
+package com.csj.git;
+
+/**
+ * @Description TODO
+ * @Author csj
+ * @Date 2022/7/3 17:19
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hellworld");
+    }
+}
