d7ac6e06b8b31962c3c5ab7f7b5f4c98fd4cc46b
csj <370916854@qq.com>
2022-07-03 17:27:09 +0800

    second commit

--- src/main/java/com/csj/git/gitTest.java
+++ src/main/java/com/csj/git/gitTest.java
@@ -8,5 +8,6 @@ package com.csj.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hellworld");
+        System.out.println("hellworld2");
     }
 }
