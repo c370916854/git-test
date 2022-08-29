87e58ffee353eb5479083d45a668b2ab367e1524
c370916854 <57848561+c370916854@users.noreply.github.com>
2022-08-29 21:55:44 +0800

    Update HelloWord.java

--- src/main/java/com/csj/HelloWord.java
+++ src/main/java/com/csj/HelloWord.java
@@ -7,5 +7,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("---------------");
         System.out.println("---------------");
+        System.out.println("sadsadjasjdasdas");
     }
 }
