217e516d0cdc5391c3623585441b9157248f182b
csj <370916864@qq.com>
2022-08-29 22:23:46 +0800

    go

--- src/main/java/com/csj/TestGit.java
+++ src/main/java/com/csj/TestGit.java
@@ -32,48 +32,4 @@ public class TestGit {
     public Integer getAge() {
         return age;
     }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getHobby() {
-        return hobby;
-    }
-
-    public void setHobby(String hobby) {
-        this.hobby = hobby;
-    }
 }
