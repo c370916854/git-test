d391c9971df3ff4350e1e93d4eacb6c0f43700f5
c370916854 <57848561+c370916854@users.noreply.github.com>
2022-08-30 22:20:00 +0800

    Update TestGit.java

--- src/main/java/com/csj/TestGit.java
+++ src/main/java/com/csj/TestGit.java
@@ -12,27 +12,4 @@ public class TestGit {
     private String gender;
     private String id;
     private String hobby;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void show(){
-
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
 }
